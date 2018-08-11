87defe377f5c3dbab588c90febfce6f96887e70f
Ian Kettlewell <ian.kettlewell@gmail.com>
2018-08-11 13:37:31 -0400

    Initial checkin

---
+++ LD42/Assets/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+    public string animalName;
+
+    Vector2 origin;
+	// Use this for initialization
+	void Start () {
+        origin = this.transform.Find("origin").localPosition;
+        AnimalColliders.CreateColliders(this.gameObject, origin, animalName);
+	}
+
+    bool moving = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            moving = false;
+        }
+
+        if (moving)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos - moveOffset;
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Rotate(1);
+            }
+        }
+    }
+
+
+    void Rotate(int direction)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.RotateAround(mousePos, Vector3.back, 90);
+        moveOffset = mousePos - transform.position;
+    }
+
+    Vector3 moveOffset;
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+       // Debug.Log("Mouse is over " + animalName);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moveOffset = mousePos - transform.position;
+            moving = true;
+        }
+    }
+
+}
---
+++ LD42/Assets/AnimalColliders.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalColliders : MonoBehaviour {
+
+    static int[,] giraffe = new int[,]
+    {
+        { 0, 1 },
+        { 0, 1 },
+        { 1, 1 },
+        { 1, 1 }
+    };
+
+    static int[,] elephant = new int[,]
+    {
+        { 0, 1 , 1},
+        { 1, 1 , 1},
+        { 1, 1 , 1}
+    };
+
+    static int[,] lion = new int[,]
+    {
+        { 0, 1},
+        { 1, 1 }
+    };
+
+    static int[,] alligator = new int[,]
+    {
+        { 1, 1, 1, 1},
+    };
+
+    static int[,] hippo = new int[,]
+{
+        { 1, 1 , 1},
+        { 1, 1 , 1}
+};
+
+    static int[,] penguin = new int[,]
+{
+        { 1 }       
+};
+
+    static int[,] trex = new int[,]
+{
+        { 0, 0, 0, 1, 1, 1 },
+        { 0, 0, 0, 1, 1, 1 },
+        { 0, 1, 1, 1, 1, 0 },
+        { 1, 1, 1, 1, 0, 0 },
+        { 0, 1, 1, 1, 0, 0 },
+};
+
+static int[,] disguised_trex = new int[,]
+{
+        { 0, 0, 0, 1, 1, 1 },
+        { 0, 0, 0, 1, 1, 1 },
+        { 0, 1, 1, 1, 1, 0 },
+        { 1, 1, 1, 1, 1, 0 },
+        { 0, 1, 1, 1, 0, 0 },
+};
+
+    static Dictionary<string, int[,]> animalNameToData = new Dictionary<string, int[,]>
+    {
+        {"giraffe", giraffe },
+        {"elephant", elephant },
+        {"lion", lion  },
+        {"hippo", hippo },
+        {"penguin", penguin },
+        {"alligator", alligator },
+        {"trex", trex},
+        {"disguised_trex", disguised_trex }
+    };
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public static float tileSize = 270;
+
+    public static void CreateColliders(GameObject gameObject, Vector2 originOffset, string animal)
+    {
+        int[,] dataArray = animalNameToData[animal];
+
+        for (int i = 0; i < dataArray.GetLength(0); ++i)
+        {
+            for (int j = 0; j < dataArray.GetLength(1); ++j)
+            {
+                if (dataArray[i, j] == 1)
+                {
+                    BoxCollider2D boxCollider = gameObject.AddComponent<BoxCollider2D>();
+                    boxCollider.offset = new Vector2(j + .5f, dataArray.GetLength(0) - i - .5f) + originOffset;
+                    boxCollider.size = new Vector2(1, 1);
+                }
+            }
+        }
+    }
+}
